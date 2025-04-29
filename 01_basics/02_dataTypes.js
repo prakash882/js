e3f27b0406bf73b3f9c43e5dbbc94c1c6c4da41c
prakash882 <pt422194@gmail.com>
2025-04-29 13:12:59 +0545

    02_dataTypes added

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+//alert(3+3)// we are using nodejs, not browser
+
+console.log(3 
+    +
+    3)// code readabilty must be high
+
+console.log(3+3)
+console.log("Prakash")
+
+let name = "Prakash"
+let age = 21
+let isLoggedIn = false
+let state = null
+
+/*
+number => 2 to power 53
+bigint
+string => ""
+boolean => true or false
+null => standalone value
+undefined => 
+symbol => unique 
+*/
+
+//object 
+console.log(typeof "Prakash") // string
+console.log(typeof age)// number
+console.log(typeof null)// object
+console.log(typeof undefined)// undefined
